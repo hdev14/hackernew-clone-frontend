d0aad520505ff965cd546cc7fd5f40ee474b8d91
Hermerson Araújo <ne.hermerson@gmail.com>
2020-11-02 10:09:11 -0300

    feat: create subscription to new votes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -2,7 +2,7 @@ import { useQuery } from '@apollo/client';
 
 import MyLink from './Link';
 import { FEED_QUERY } from '../graphql/queries';
-import { NEW_LINKS_SUBSCRIPTION } from '../graphql/subscriptions';
+import { NEW_LINKS_SUBSCRIPTION, NEW_VOTES_SUBSCRIPTION } from '../graphql/subscriptions';
 
 const LinkList = () => {
   const {
@@ -33,10 +33,17 @@ const LinkList = () => {
     });
   };
 
+  const subscribeToNewVotes = () => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION,
+    });
+  };
+
   if (loading) return <h1>loading...</h1>;
   if (error) return <span>{error}</span>;
 
   subscribeToNewLinks();
+  subscribeToNewVotes();
 
   return (
     <div>
--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -20,3 +20,30 @@ export const NEW_LINKS_SUBSCRIPTION = gql`
     }
   }
 `;
+
+export const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
